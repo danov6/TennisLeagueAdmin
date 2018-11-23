36d0c19e82f590cb1055b54961b92f7d13ddeaff
Gio <danov6@aol.com>
2018-11-23 07:30:16 -0800

    Added Player Data
    Added player data and updated logic for App.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,53 @@
 import React, { Component } from 'react';
-//import './data/players';
+import PlayerData from './data/PlayerData.json';
 
-const players = [
-  {key: '1000', rank:'1', name: 'Gio Valdez', team: 'Southern California', conference: 'WCC', rating: '9'},
-  {key: '2000', rank:'2', name: 'Gio Valdez', team: 'Southern California', conference: 'WCC', rating: '8'},
-  {key: '3000', rank:'3', name: 'Gio Valdez', team: 'Southern California', conference: 'WCC', rating: '7'}
-];
-
-function Players(props) {
-  const players = props.players.map((player) =>
-    <tr key={player.key}>
-      <td>{player.rank}</td>
-      <td>{player.name}</td>
-      <td>{player.team}</td>
-      <td>{player.conference}</td>
-      <td>{player.rating}</td>
-      <td>
-        <button type="button" className="btn btn-default btn-sm">
-          <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span> Edit
-        </button>
-      </td>
-    </tr>
-  );
-  return (
-    <tbody>{players}</tbody>
-  );
-}
+var i = 1;
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {      
+      PlayerData: PlayerData,
+      date: new Date()
+    };
+  }
+
   render() {
+
+    const players = PlayerData.map((player) => {
+      return (
+        <tr key={player.id}>
+            <td>{i++}</td>
+            <td>{player.name}</td>
+            <td>{player.team}</td>
+            <td>{player.conference}</td>
+            <td>{player.pr}</td>
+            <td>{player.points}</td>
+            <td>
+              <button type="button" className="btn btn-default btn-sm">
+                <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span> Edit
+              </button>
+          </td>
+        </tr>
+      );
+    });
+
     return (
       <table className="table table-striped">
         <thead>
           <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Team</th>
-            <th>Conference</th>
-            <th>Rating</th>
-            <th></th>
+            <th><a href="#">#</a></th>
+            <th><a href="#">Name</a></th>
+            <th><a href="#">Team</a></th>
+            <th><a href="#">Conference</a></th>
+            <th><a href="#">PR</a></th>
+            <th><a href="#">Points</a></th>
           </tr>
         </thead>
-        < Players players={players} />
+        <tbody>
+          {players}
+        </tbody>
       </table>
     );
   }

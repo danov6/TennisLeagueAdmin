9c0e7c8e0a515f432e488bc6416e1df987a4eaa6
danov6 <glvaldez@callutheran.edu>
2019-04-07 13:46:00 -0700

    update the server file, using mongodb instead

--- src/Server.js
+++ src/Server.js
@@ -1,28 +1,87 @@
-const express = require('express');
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const API_PORT = 3001;
 const app = express();
-const port = 5000;
+const router = express.Router();
 
-const bodyParser = require('body-parser');
+// this is our MongoDB database
+const dbRoute = "mongodb+srv://glvaldez:Giordano1!@player-data-app-bdkhh.mongodb.net/test?retryWrites=true";
+//mongodb+srv://glvaldez:<PASSWORD>@player-data-app-bdkhh.mongodb.net/test?retryWrites=true
+const playerRoutes = require("./api/Routes/players");
 
-var mysql = require('mysql');  
+// connects our back end code with the database
+mongoose.connect(
+  dbRoute,
+  {
+   useNewUrlParser: true
+  }
+);
 
-var connection = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'root',
-	password : 'Giordano1!',
-	database : 'tennisplayers'
+let db = mongoose.connection;
+
+db.once("open", () => console.log("connected to the database"));
+
+// checks if connection with the database is successful
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// (optional) only made for logging and
+// bodyParser, parses the request body to be a readable json format
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
 });
 
-app.get('/players', (req, res) => {
-	connection.connect();
-	connection.query('SELECT * FROM tennisplayers.players LIMIT 0, 10', function(err, rows, fields) {
-		if (!err) {
-        	res.send(JSON.stringify(rows));
-		} else {
-			console.log('Error while performing Query.');
-		}
-	});
-	connection.end();
+// Routes which should handle requests
+app.use("/players", playerRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
 });
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+
+// launch our backend into a port
+app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

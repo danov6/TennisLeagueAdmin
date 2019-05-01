6519bfaadb5194c40576dfcb8c1c24306e0f1657
danov6 <glvaldez@callutheran.edu>
2019-04-30 20:55:59 -0700

    added team data analytics

--- src/components/tables/TeamRankingTable.js
+++ src/components/tables/TeamRankingTable.js
@@ -23,6 +23,8 @@ export default class RankingTable extends React.Component {
                 <td className="playertablefield" onClick={this.handleTeamClick} data-value={team.name} >{ team.name }</td>
                 <td>{ ConfAbbreviations[team.conference].Name.replace(' Conference', '') }</td>
                 <td>{ team.points }</td>
+                <td>{ team.depth }</td>
+                <td><b>{ (team.points + team.depth) / 2 }</b></td>
             </tr>
     }); 
 
@@ -36,7 +38,9 @@ export default class RankingTable extends React.Component {
                             <th>#</th>
                             <th>Team</th>
                             <th>Conference</th>
-                            <th>Overall Rating</th>
+                            <th>Points</th>
+                            <th>Depth</th>
+                            <th>Rating</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -51,22 +55,43 @@ export default class RankingTable extends React.Component {
 function sortTeams (playerData){
     let teams = [];
     for(var i = 0; i < Object.keys(TeamAbbreviations).length; i++){
+        let ratings = [];
         var abbrev = Object.keys(TeamAbbreviations)[i];
         var team = {
             name: TeamAbbreviations[abbrev].Name,
             conference: TeamAbbreviations[abbrev].Conf,
-            points: 0
+            points: 0,
+            depth: 0,
+            rating: 0
         };
         for(var j = 0; j < playerData.length; j++){
             if(abbrev === playerData[j].team){
-                team.points += playerData[j].pr === "" ? 0 : parseInt(playerData[j].pr);
+                var playerPoints = playerData[j].pr === "" ? 0 : parseInt(playerData[j].pr);
+                team.points += playerPoints;
+                ratings.push(playerPoints);
             }
         }
+        team.depth = getDepthRating(ratings);
+        team.rating = (team.points + team.depth) / 2;
         teams.push(team);
     }
 
     teams = _.orderBy(teams, (item) => {
-        return item['points']
+        return item['rating']
     }, 'desc');
     return teams;
+}
+function getDepthRating (ratings){
+    var sorted = ratings.sort(sorter);
+    var depthRating = 0;
+    sorted.pop();
+    sorted.pop();
+
+    for(var i = 0; i < sorted.length; i++){
+        depthRating += sorted[i];
+    }
+    return depthRating;
+}
+function sorter(a, b){
+    return a - b;
 }
